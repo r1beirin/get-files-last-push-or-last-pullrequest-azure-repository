d6cb6d54ce7a334ceb4e9fe8780c1da66e3390cb
Mateus Ribeiro Vaz Pereira <mateus.pereira@servicesec.com.br>
2024-05-27 11:01:52 -0300

    refactor: add function to get modified from Push

--- main.py
+++ main.py
@@ -16,6 +16,27 @@ def createConnection(personalAccessToken, organizationURL):
 
     return connection
 
+def getModifiedFromPush(pushes, gitClient, projectName, repoID, modifiedFiles):
+    lastPushID = pushes[0].push_id
+
+    push_details = gitClient.get_push(project=projectName, repository_id=repoID, push_id=lastPushID)
+
+    commits = push_details.commits
+
+    for commit in commits:
+        commitID = commit.commit_id
+
+        commitDetails = gitClient.get_commit(project=projectName, repository_id=repoID, commit_id=commitID) 
+
+        if commitDetails:
+            changes = gitClient.get_changes(project=projectName, repository_id=repoID, commit_id=commitID)
+            if changes:
+                for change in changes.changes:
+                    if not change['item'].get('isFolder'):
+                        nameFile = change['item']['path'].lstrip('/')
+                        print(f'Arquivo modificado: {nameFile}')
+                        modifiedFiles.append(nameFile)
+
 def main():
     parser = argparse.ArgumentParser('This script create a zip with the modified files in last push from Azure Repo.')
     parser.add_argument('-pat', '--pat', help='Personal Access Token', required=True)
@@ -28,6 +49,7 @@ def main():
     organizationURL = args.orgurl
     projectName = args.projectname
     repoID = args.repoid
+    modifiedFiles = []
 
     connection = createConnection(personalAccessToken, organizationURL)
 
@@ -36,26 +58,7 @@ def main():
     pushes = getAllPushes(gitClient, projectName, repoID)
 
     if pushes:
-        modifiedFiles = []
-        lastPushID = pushes[0].push_id
-
-        push_details = gitClient.get_push(project=projectName, repository_id=repoID, push_id=lastPushID)
-
-        commits = push_details.commits
-
-        for commit in commits:
-            commitID = commit.commit_id
-
-            commitDetails = gitClient.get_commit(project=projectName, repository_id=repoID, commit_id=commitID) 
-
-            if commitDetails:
-                changes = gitClient.get_changes(project=projectName, repository_id=repoID, commit_id=commitID)
-                if changes:
-                    for change in changes.changes:
-                        if not change['item'].get('isFolder'):
-                            nameFile = change['item']['path'].lstrip('/')
-                            print(f'Arquivo modificado: {nameFile}')
-                            modifiedFiles.append(nameFile)        
+        getModifiedFromPush(pushes, gitClient, projectName, repoID, modifiedFiles)
 
     if modifiedFiles:
         with zipfile.ZipFile("modified_files.zip", "w") as zipf:
